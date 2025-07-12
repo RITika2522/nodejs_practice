86b662ce759033a3dffa17fca1b158b740e70f4c
RITika2522 <ritikachandak2502@gmail.com>
2025-07-13 01:43:22 +0530

    understanding url

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
+
 
 const myserver = http.createServer((req, res) => {
+    if (req.url === "/favicon.ico") return res.end();
     const log = `${Date.now()}: ${req.url} New Request received\n`;
+    const myurl = url.parse(req.url,true);
+    console.log(myurl);
     fs.appendFile('log.txt',log, (err,data) => {
-        switch(req.url){
+        switch(myurl.pathname){
             case '/': res.end("Home Page");
                 break;
-            case '/about': res.end("I am Ritika Chandak");
+            case '/about': 
+                const qp = myurl.query.myname;
+                res.end(`HI ${qp}`);
                 break;
             default: res.end("404 Not Found");
         }
