3631449f5b6d9e9a4725d9e83f6ec772502b01c0
RITika2522 <ritikachandak2502@gmail.com>
2025-07-13 01:09:20 +0530

    routing server and logging of requests

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
 const myserver = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Hello from server!');
+    const log = `${Date.now()}: ${req.url} New Request received\n`;
+    fs.appendFile('log.txt',log, (err,data) => {
+        switch(req.url){
+            case '/': res.end("Home Page");
+                break;
+            case '/about': res.end("I am Ritika Chandak");
+                break;
+            default: res.end("404 Not Found");
+        }
+    })
 });
 
 myserver.listen(8000, () => console.log("New Server started!"));
