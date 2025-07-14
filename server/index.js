4b81d4ed1e0a7187a859bbe7f57ca53c8fbfc395
RITika2522 <ritikachandak2502@gmail.com>
2025-07-14 16:49:13 +0530

    URL Handling and http request methods

--- server/index.js
+++ server/index.js
@@ -5,17 +5,28 @@ const url = require('url');
 
 const myserver = http.createServer((req, res) => {
     if (req.url === "/favicon.ico") return res.end();
-    const log = `${Date.now()}: ${req.url} New Request received\n`;
+    const log = `${Date.now()}: ${req.method} ${req.url} New Request received\n`;
     const myurl = url.parse(req.url,true);
     console.log(myurl);
     fs.appendFile('log.txt',log, (err,data) => {
         switch(myurl.pathname){
-            case '/': res.end("Home Page");
+            case '/':
+                if(req.method ==='GET'){
+                    res.end("Home Page");
+                } 
                 break;
             case '/about': 
                 const qp = myurl.query.myname;
                 res.end(`HI ${qp}`);
                 break;
+            case '/signup':
+                if(req.method ==='GET'){
+                    res.end('This is a Sign up Form');
+                }
+                else if(req.method === 'POST'){
+                    //DB QUERY
+                    res.end("SUCCESS");
+                }
             default: res.end("404 Not Found");
         }
     })
